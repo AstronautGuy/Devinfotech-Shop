98f721ba8ddfbd9636307fc799f28b22cfc93aef
AstronautGuy <theastronautguy2@gmail.com>
2025-06-15 12:01:47 +0530

    eslint firse maa chuda rha hai

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,13 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    // ⬇️ THIS BLOCK is needed to enable disable-comments
+    linterOptions: {
+      allowInlineConfig: true,
+      reportUnusedDisableDirectives: false,
+    },
+  },
 ];
 
 export default eslintConfig;
